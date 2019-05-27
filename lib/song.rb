ce3bb0c6cca03efa9775d6ead56b1ab54b5cdea6
Jess Rezac <jess.rezac@gmail.com>
2019-05-27 03:45:30 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,7 @@ class Song
   @@count = 0
   @@artists = []
   @@genres = []
-  @@genre_count = {}
-  @@artist_count = {}
-  
+
   attr_accessor :name, :artist, :genre
   
   def initialize(name, artist, genre)
@@ -32,6 +30,8 @@ class Song
   end
   
   def self.genre_count
+    @@genre_count = {}
+    
     @@genres.each do |genre|
       @@genre_count[genre] ||= 0
       @@genre_count[genre] += 1
@@ -41,6 +41,8 @@ class Song
   end
   
   def self.artist_count
+    @@artist_count = {}
+    
     @@artists.each do |artist|
       @@artist_count[artist] ||= 0
       @@artist_count[artist] += 1
