6e9091458130839d25387de6ec77242922f1b12c
Annia Forney <anniaforney@gmail.com>
2019-06-12 16:01:22 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,14 @@
 class Song
-  attr_accessor :song :artist :genre
+  attr_accessor :name, :artist, :genre
 
-  @@song_count = 1
+  @@count = 0
+  @@artists = []
+  @@genres = [] 
 
-  def initialize
-    @@song_count += 1
-  end
-
-  def self.count
-    @@song_count
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
   end
 
 end
