db53fcab52e9366d84601396df4230331a5e3a30
Annia Forney <anniaforney@gmail.com>
2019-06-12 16:04:20 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -19,9 +19,18 @@ class Song
   end
 
   def self.artists
-    
+    artists.uniq
   end
 
+  def self.genres
+    genres.uniq
+  end
+
+  def self.genre_count
+  end
+
+  def self.artist_count
+  end 
 
 
 end
